7f6de214b948fdef43edf23ad55dc907a9193b72
binhtt2 <thanhbinhtruong678@gmail.com>
2020-08-24 20:17:34 +0900

    commit

---
+++ terminal.py
@@ -0,0 +1,38 @@
+import os
+import platform
+
+print(platform.system())
+print(platform.release())
+print(os.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pyautogui.FAILSAFE = False
+# print(pyautogui.position())
+# x = GetSystemMetrics(0)
+# y = GetSystemMetrics(1)
+# pyautogui.typewrite(['win'])
+# # pyautogui.moveTo(0, y-1, 1)
+# # pyautogui.click()
+# # sleep(1)
+# # pyautogui.typewrite('Google Chrome')
+# # sleep(1)
+# # pyautogui.typewrite(['windows'])
+
+ 
